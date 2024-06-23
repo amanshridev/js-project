8cc8e9edfa9c92579e6ed69955b5859ff5cc5313
amanshridev <mailtoamanshri@proton.me>
2024-06-23 21:17:36 +0530

    checked js file working

--- project_5/index.js
+++ project_5/index.js
@@ -1,1 +1,8 @@
 console.log('oh yeah working')
+
+let randomNumber = parseInt(Math.random() * 100 + 1);
+
+console.log(randomNumber);
+
+let submit =  document.querySelector('#submit-btn');
+let inputValue = document.querySelector('#input-value')
